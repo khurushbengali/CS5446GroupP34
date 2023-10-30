fc1c6c79ed87efa2f9ea53020e877b1a2018004d
shawn-t <shawnteoxuanwei@gmail.com>
2023-10-31 00:36:07 +0800

    update README.md w func explanation

--- README.md
+++ README.md
@@ -4,3 +4,25 @@ Classing Planning to solve a 2x2x2 Rubiks Cube [A-Star Algorithm]
 ├── CubeLabels.py [Added labelling for cube sides and rotations]
 └── README.md
 ```
+
+function make2dCube()
+takes in a list of 24 elements and returns a two-dimensional list:
+input: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+output: [[ 0,  1,  2,  3],
+         [ 4,  5,  6,  7],
+         [ 8,  9, 10, 11],
+         [12, 13, 14, 15],
+         [16, 17, 18, 19],
+         [20, 21, 22, 23]]
+
+function print2dCube()
+takes in a list of 24 elements and outputs it in the following form for easier checking:
+input: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+
+prints:
+['', '', 0, 1, '', '', '', '']
+['', '', 2, 3, '', '', '', '']
+[4, 5, 8, 9, 12, 13, 16, 17]
+[6, 7, 10, 11, 14, 15, 18, 19]
+['', '', 20, 21, '', '', '', '']
+['', '', 22, 23, '', '', '', '']
--- main.py
+++ main.py
@@ -41,8 +41,36 @@ def print2dCube(cube):
     for i in range(len(x)):
         print(x[i])
 
-def printCube(x):
-    print(x)
+def printCube(c):
+    print(c)
+
+    print("      ", c[0], c[1])
+    print("      ", c[2], c[3])
+
+    # x[2][0] = c[1][0]
+    # x[2][1] = c[1][1]
+    # x[3][0] = c[1][2]
+    # x[3][1] = c[1][3]
+
+    # x[2][2] = cube[2][0]
+    # x[2][3] = cube[2][1]
+    # x[3][2] = cube[2][2]
+    # x[3][3] = cube[2][3]
+
+    # x[2][4] = cube[3][0]
+    # x[2][5] = cube[3][1]
+    # x[3][4] = cube[3][2]
+    # x[3][5] = cube[3][3]
+
+    # x[2][6] = cube[4][0]
+    # x[2][7] = cube[4][1]
+    # x[3][6] = cube[4][2]
+    # x[3][7] = cube[4][3]
+
+    # x[4][2] = cube[5][0]
+    # x[4][3] = cube[5][1]
+    # x[5][2] = cube[5][2]
+    # x[5][3] = cube[5][3]
 
 def make2dCube(x):
     a = [[],[],[],[],[],[]]
@@ -60,7 +88,7 @@ def main():
     x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
     # x = [2,  0,  3,  1, 20, 21,  6,  7,  4,  5, 10, 11, 12, 13, 14, 15, 8, 9, 18, 19, 16, 17, 22, 23]
     cube = make2dCube(x)
-    # print(cube)
+    printCube(x)
 
     print2dCube(cube)
     # printCube(x)
